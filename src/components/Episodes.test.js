a8f3c86417485b48d7f0f0124b143ac03d7ae756
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-10-12 09:30:37 -0700

    Test Commit

---
+++ src/components/Episodes.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react"
+import Episodes from "./Episodes"
+
+
+test("Re-renders correctly with list of episodes", () => {
+    //arange
+    render(<Episodes />)
+    //act
+    
+    //assert
+
+})
